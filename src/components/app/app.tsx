b2b1bd1a1929c535a88206f8702c87ec03639c97
Alexander <alex.kadoh@mail.ru>
2022-03-24 17:47:31 +0700

    config for gh pages

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Main from '../main/main';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route, HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from '../../store/store';
 import NotFound from '../not-found/not-found';
 
 function App():JSX.Element {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={'/'} element={<Main/>}/>
-          <Route path={'*'} element={<NotFound/>}/>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+      <HashRouter basename={process.env.PUBLIC_URL}>
+          <Provider store={store}>
+              <Routes>
+                  <Route path={'/'} element={<Main/>}/>
+                  <Route path={'*'} element={<NotFound/>}/>
+              </Routes>
+          </Provider>
+      </HashRouter>
   );
 }
 
